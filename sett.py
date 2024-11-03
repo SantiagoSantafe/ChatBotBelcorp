128ec479ac3dfadc408719e6845076214528a462
Santiago <andressasi@unisabana.edu.co>
2024-11-02 22:10:43 -0500

    initial commit

---
+++ sett.py
@@ -0,0 +1,23 @@
+token = 'bigdateros'
+
+whatsapp_token = 'EAAbsmFU0KS4BO5rGAQpXS83QuTMRTDk6ieFCcj57SEZCXXBnZAVWjXFptBRzkbcQIQupvHvqA7Au7ZAs0k971MjfuJzZCd4N1wF10VEfGu2p8jducdedMn2IUOZCnXwKPgfgmKF1oJmOvB3AT2xnAbYwqbjFORfvUHJ6m15wm2ARLpqW9ccIrmlNMQqMCzEd9jlNiK0wtWEiSzpsze08NnVPPdwZDZD'
+
+whatsapp_url = 'https://graph.facebook.com/v20.0/503431269513746/messages'
+
+stickers = {
+    "poyo_feliz": 984778742532668,
+    "perro_traje": 1009219236749949,
+    "perro_triste": 982264672785815,
+    "pedro_pascal_love": 801721017874258,
+    "pelfet": 3127736384038169,
+    "anotado": 24039533498978939,
+    "gato_festejando": 1736736493414401,
+    "okis": 268811655677102,
+    "cachetada": 275511571531644,
+    "gato_juzgando": 107235069063072,
+    "chicorita": 3431648470417135,
+    "gato_triste": 210492141865964,
+    "gato_cansado": 1021308728970759
+}
+
+document_url = "https://www.africau.edu/images/default/sample.pdf"
